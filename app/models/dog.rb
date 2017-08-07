2f2f9f480f99f2007a06ee917399ee9b3230e476
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-08-07 17:26:00 -0400

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+
+@@all = []
+	attr_reader :breed
+	attr_accessor :age, :name
+
+	def initialize(name,breed,age)
+		@name = name
+		@breed = breed
+		@age = age
+		@@all << self
+	end
+
+	def self.all 
+		@@all 
+	end
+end
